3d107bd5baaa38c8b57100ff1c76b363782dcab3
itsBabuaa <atharvsingh.edu@gmail.com>
2025-01-04 17:27:28 +0530

    Create Two Sum.py

---
+++ Array/Two Sum.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/two-sum/
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            moreriq = target - num
+            if moreriq in hashmap:
+                return [hashmap[moreriq], i]
+            hashmap[num] = i
+        return [-1,-1]
+
+# Time: O(n)
+# Space: O(n)
