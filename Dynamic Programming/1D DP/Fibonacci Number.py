d8497ad974eec3b409e267762ba5d7322f941962
Atharv Singh <atharvsingh.edu@gmail.com>
2025-03-27 21:28:27 +0530

    Update Fibonacci Number.py

--- Dynamic Programming/1D DP/Fibonacci Number.py
+++ Dynamic Programming/1D DP/Fibonacci Number.py
@@ -5,6 +5,7 @@ F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
 '''
 
+# Memoization Solution
 class Solution:
     def fib(self, n: int) -> int:
         dp = [-1] * (n + 1)
@@ -20,3 +21,25 @@ class Solution:
         
         dp[n] = self.fibHelper(n-1, dp) + self.fibHelper(n-2, dp)
         return dp[n]
+#Time: O(N)
+#Space: O(N) + O(N) ≈ O(N)
+
+# Tabulation Solution
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        dp = [-1] * (n + 1)
+
+        dp[0] = 0
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+#Time: O(N)
+#Space: O(N)
