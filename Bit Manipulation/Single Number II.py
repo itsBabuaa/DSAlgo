ea5395fab6665bc33647cfe5fc8aaab0d661e086
Atharv Singh <atharvsingh.edu@gmail.com>
2025-09-05 16:54:04 +0530

    Create Single Number II.py

---
+++ Bit Manipulation/Single Number II.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/single-number-ii/description/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones, twos = 0, 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
