f6cd6fcc0c09e015e50873b0ee419acaf0575818
Atharv Singh <atharvsingh.edu@gmail.com>
2025-09-05 17:15:43 +0530

    Create Single Number III.py

---
+++ Bit Manipulation/Single Number III.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/single-number-iii/description/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for num in nums:
+            xor ^= num
+
+        rightmost = xor & -xor
+        xor1, xor2 = 0, 0
+
+        for num in nums:
+            if rightmost & num:
+                xor1 ^= num
+            else:
+                xor2 ^= num
+
+        return [xor1, xor2]
